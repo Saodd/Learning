bca75016c4918e890af48db541d0f28f7a021204
Lewin <393070029@qq.com>
2019-05-14 21:16:57 +0800

    2019-05-14

---
+++ golang/0002_Exercise_Loops_Functions.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+import "math"
+
+/*
+Calc *sqrt()*.
+function is:
+	guess z
+	loop z -= (z*z - x) / (2*z)
+	return z
+*/
+
+func mysqrt(x float64) (z float64) {
+	delta := 0.000000001
+	z = 1.0
+	times := 0
+	for y := 0.0; (y-z > delta) || (z-y > delta); times++ {
+		y = z
+		z = y - (y*y-x)/(2*y)
+	}
+	fmt.Printf("mysqrt  Loop %d times:  ", times)
+	return z
+}
+
+func mysqrt2(x float64) (z float64) {
+	delta := 0.000000001
+	z = x / 2
+	times := 0
+	for y := 0.0; (y-z > delta) || (z-y > delta); times++ {
+		y = z
+		z = y - (y*y-x)/(2*y)
+	}
+	fmt.Printf("mysqrt2 Loop %d times:  ", times)
+	return z
+}
+
+func main() {
+	x := 564684685136543841.0
+	fmt.Println("The Standard Answer is:", math.Sqrt(x))
+	fmt.Println(mysqrt(x))
+	fmt.Println(mysqrt2(x))
+}
