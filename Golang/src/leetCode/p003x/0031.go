8fba2a5c03e20971c43df55b4e58d745471e448e
Lewin Lan <393070029@qq.com>
2019-08-25 17:52:35 +0800

    LeetCode 46

--- Golang/src/leetCode/p003x/0031.go
+++ Golang/src/leetCode/p003x/0031.go
@@ -1,7 +1,7 @@
 package p003x
 
 import (
-    "learnAlgo"
+	"learnAlgo"
 )
 
 /*
@@ -22,72 +22,72 @@ import (
 链接：https://leetcode-cn.com/problems/next-permutation
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
-func nextPermutation(nums []int) {
-    if len(nums) < 2 {
-        return
-    }
-    var top int
-    for top = len(nums) - 1; top > 0; top-- {
-        if nums[top-1] < nums[top] {
-            break
-        }
-    }
-    if top == 0 {
-        swapSortInt(nums)
-        return
-    }
-    exchId := indexOfMinGreater(nums[top:], nums[top-1]) + top
-    nums[top-1], nums[exchId] = nums[exchId], nums[top-1]
-    swapSortInt(nums[top:])
-    return
+func NextPermutation(nums []int) {
+	if len(nums) < 2 {
+		return
+	}
+	var top int
+	for top = len(nums) - 1; top > 0; top-- {
+		if nums[top-1] < nums[top] {
+			break
+		}
+	}
+	if top == 0 {
+		swapSortInt(nums)
+		return
+	}
+	exchId := indexOfMinGreater(nums[top:], nums[top-1]) + top
+	nums[top-1], nums[exchId] = nums[exchId], nums[top-1]
+	swapSortInt(nums[top:])
+	return
 }
 
 // 二分查找
 func indexOfMinGreater(nums []int, target int) int {
-    var gt, lt int
-    for gt, lt = 0, len(nums); lt-gt > 1; {
-        mid := (gt + lt) / 2
-        if nums[mid] > target {
-            gt = mid
-        } else {
-            lt = mid
-        }
-    }
-    return gt
+	var gt, lt int
+	for gt, lt = 0, len(nums); lt-gt > 1; {
+		mid := (gt + lt) / 2
+		if nums[mid] > target {
+			gt = mid
+		} else {
+			lt = mid
+		}
+	}
+	return gt
 }
 
 // 交换排序
 func swapSortInt(nums []int) {
-    for i, j := 0, len(nums)-1; i < j; {
-        nums[i], nums[j] = nums[j], nums[i]
-        i++
-        j--
-    }
+	for i, j := 0, len(nums)-1; i < j; {
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
+	}
 }
 
 func nextPermutation1(nums []int) {
-    if len(nums) < 2 {
-        return
-    }
-    var top int
-    // 找到顶点，即降序排列的头部
-    for top = len(nums) - 1; top > 0; top-- {
-        if nums[top-1] < nums[top] {
-            break
-        }
-    }
-    if top == 0 {
-        learnAlgo.QuickSortInt(nums) // 快速排序函数
-        return
-    }
-    // 在右边数组中找到一个值去跟左边交换
-    var targetNum, updateIndex int = nums[top-1], top
-    for i := len(nums) - 1; i > top; i-- {
-        if nums[i] > targetNum && nums[i] < nums[updateIndex] {
-            updateIndex = i
-        }
-    }
-    nums[top-1], nums[updateIndex] = nums[updateIndex], nums[top-1]
-    learnAlgo.QuickSortInt(nums[top:]) // 快速排序函数
-    return
+	if len(nums) < 2 {
+		return
+	}
+	var top int
+	// 找到顶点，即降序排列的头部
+	for top = len(nums) - 1; top > 0; top-- {
+		if nums[top-1] < nums[top] {
+			break
+		}
+	}
+	if top == 0 {
+		learnAlgo.QuickSortInt(nums) // 快速排序函数
+		return
+	}
+	// 在右边数组中找到一个值去跟左边交换
+	var targetNum, updateIndex int = nums[top-1], top
+	for i := len(nums) - 1; i > top; i-- {
+		if nums[i] > targetNum && nums[i] < nums[updateIndex] {
+			updateIndex = i
+		}
+	}
+	nums[top-1], nums[updateIndex] = nums[updateIndex], nums[top-1]
+	learnAlgo.QuickSortInt(nums[top:]) // 快速排序函数
+	return
 }
